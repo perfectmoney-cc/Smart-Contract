eb81393070a3ce3c29f016ae179e93c5e3b36668
Perfect Money <perfectmoney.cc@gmail.com>
2025-12-02 19:41:18 +0800

    Update PerfectMoney.sol

--- PerfectMoney.sol
+++ PerfectMoney.sol
@@ -1,43 +1,155 @@
+/**
+ *Submitted for verification at BscScan.com on 2025-12-01
+*/
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+/**
+ * @dev Minimal OpenZeppelin ERC20 implementation 
+ */
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender,address recipient,uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner,address indexed spender,uint256 value);
+}
 
-contract PerfectMoney is ERC20, ERC20Burnable, Ownable, ReentrancyGuard {
-    uint256 public constant MAX_SUPPLY = 100_000_000_000 * 10**18;
-    bool public mintingDisabled = false;
+/**
+ * @dev Ownable with manual renounce
+ */
+abstract contract Ownable {
+    address private _owner;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    constructor() ERC20("Perfect Money", "PM") Ownable(msg.sender) {
-        _mint(msg.sender, MAX_SUPPLY);
+    constructor() {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
     }
 
-    /// @notice Permanently disables the mint function. Cannot be undone.
-    function disableMintingForever() external onlyOwner {
-        mintingDisabled = true;
+    function owner() public view returns (address) {
+        return _owner;
     }
 
-    /// @notice Mint function, permanently disabled once `disableMintingForever` is called
-    function mint(address to, uint256 amount) external onlyOwner {
-        require(!mintingDisabled, "Minting has been permanently disabled");
-        require(totalSupply() + amount <= MAX_SUPPLY, "Max supply exceeded");
-        _mint(to, amount);
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Ownable: caller is not the owner");
+        _;
     }
 
-    /// @notice Override transfer to prevent reentrancy attacks
-    function transfer(address to, uint256 amount) 
-        public override nonReentrant returns (bool) 
-    {
-        return super.transfer(to, amount);
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
     }
 
-    /// @notice Override transferFrom to prevent reentrancy attacks
-    function transferFrom(address from, address to, uint256 amount) 
-        public override nonReentrant returns (bool) 
-    {
-        return super.transferFrom(from, to, amount);
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is zero");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+/**
+ * @title ERC20
+ */
+contract ERC20 is IERC20 {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+
+    uint256 public override totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(string memory name_, string memory symbol_) {
+        name = name_;
+        symbol = symbol_;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner,address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender,uint256 amount) public override returns (bool) {
+    _approve(msg.sender, spender, amount);
+    return true;
+    }
+
+    function transferFrom(address sender,address recipient,uint256 amount) public override returns (bool) {
+        uint256 curAllowance = _allowances[sender][msg.sender];
+        require(curAllowance >= amount, "ERC20: insufficient allowance");
+        _approve(sender, msg.sender, curAllowance - amount); // now works
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from zero");
+        require(spender != address(0), "ERC20: approve to zero");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _transfer(address sender,address recipient,uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from zero");
+        require(recipient != address(0), "ERC20: transfer to zero");
+        uint256 bal = _balances[sender];
+        require(bal >= amount, "ERC20: insufficient balance");
+        unchecked {
+            _balances[sender] = bal - amount;
+            _balances[recipient] += amount;
+        }
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account,uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to zero");
+        totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account,uint256 amount) internal {
+        uint256 bal = _balances[account];
+        require(bal >= amount, "ERC20: burn exceeds balance");
+        unchecked {
+            _balances[account] = bal - amount;
+            totalSupply -= amount;
+        }
+        emit Transfer(account, address(0), amount);
+    }
+}
+
+/**
+ * @title Perfect Money Token (PM)
+ * @notice Ownable, fully transparent, 100B total supply, burnable
+ */
+contract PerfectMoneyToken is ERC20, Ownable {
+
+    uint256 public constant INITIAL_SUPPLY = 100_000_000_000 * 1e18; // 100B tokens
+
+    constructor() ERC20("Perfect Money", "PM") {
+        // Mint total supply to deployer (owner)
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    /**
+     * @notice Optional user burn
+     */
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
     }
 }
